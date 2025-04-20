829a14c340f66f4ee6f11578f20c7d9d8aa77780
abhishek-7081 <luckydwivedi7081@gmail.com>
2025-04-20 20:20:13 +0530

    Timeline

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,16 @@
 import CustomCursor from "@/components/CustomCursor";
 import Footer from "@/components/Footer";
+import CyberpunkTimeline from "@/components/TImeline";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <CustomCursor />
-{/* put every thing below this  */}
+      {/* put every thing below this  */}
 
+      
+      <CyberpunkTimeline />
 
       <Footer />
 
