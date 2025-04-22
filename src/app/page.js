2b4748a39d76d37b24c1d6e630eadeb93a83a635
Vishal-RAJ-DEV <user.vishalraj140706@gmail.com>
2025-04-22 10:46:34 +0530

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,37 +1,24 @@
 import AboutUs from "@/components/About";
 import CustomCursor from "@/components/CustomCursor";
 import Footer from "@/components/Footer";
-<<<<<<< HEAD
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";  // Changed import name
-=======
 import CyberpunkTimeline from "@/components/TImeline";
->>>>>>> upstream/main
 import Image from "next/image";
 
 export default function Page() {  // Renamed function to avoid conflict
   return (
-<<<<<<< HEAD
     <div className="relative flex flex-col items-center justify-center min-h-screen bg-[#71225311] text-white bg-no-repeat w-full overflow-x-hidden">
+      <CustomCursor />
       <Navbar />
       <Hero />
+        
+      <CyberpunkTimeline />
+      <AboutUs/>
       <Footer />
 
     </div>
-=======
-    <>
-      <CustomCursor />
-      {/* put every thing below this  */}
-
-      
-      <CyberpunkTimeline />
-      <AboutUs/>
-
-      <Footer />
-
-
-    </>
->>>>>>> upstream/main
+    
 
   );
 }
